dfad72779655526b00411284716739ed3c5dc80b
AlijonTursunboyev <147589319+AlijonTursunboyev@users.noreply.github.com>
2024-05-03 22:17:42 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <limits> 
-using namespace std; 
-vector<vector<int>> graph; 
-vector<int> dist;
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> graphic;
+vector<int> distance;
 void bfs(int source) {
-  queue<int> q;
-  q.push(source);
-  dist[source] = 0; 
-  while (!q.empty()) { 
-    int u = q.front(); 
-    q.pop(); for (int v : graph[u]) {
-      if (dist[v] == -1) { 
-        dist[v] = dist[u] + 1; 
-        q.push(v); } 
-    } 
-  }
-} 
+    queue<int> q;
+    
+    q.push(source);
+    distance[source] = 0;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop(); 
+        for (int v : graphic[u]) {
+            if (distance[v] == -1) {
+                distance[v] = distance[u] + 1;
+                q.push(v); }
+        }
+    }
+}
 int main() {
-  int n, m;
-  cin >> n >> m;
-  graph.resize(n);
-  dist.resize(n, -1);
-  for (int i = 0; i < m; i++) {
-    int u, v;
-    cin >> u >> v;
-    graph[u].push_back(v); 
-    graph[v].push_back(u); 
-  } 
-  bfs(0);
-  for (int i = 0; i < n; i++) { 
-    if (dist[i] == -1) { 
-      cout << -1 << " "; 
-    } 
-    else { cout << dist[i] << " ";
-         } 
-  } 
-  return 0; 
+    int a, b;
+    cin >> a >> b;
+    graphic.resize(a);
+    distance.resize(a, -1);
+    for (int i = 0; i < b; i++) {
+        int u, v;
+        cin >> u >> v;
+        graphic[u].push_back(v);
+        graphic[v].push_back(u);
+    }
+    bfs(0);
+    for (int i = 0; i < a; i++) {
+        if (distance[i] == -1) {
+            cout << -1 << " ";
+        }
+        else { cout << distance[i] << " ";
+        }
+    }
+    return 0;
 }
